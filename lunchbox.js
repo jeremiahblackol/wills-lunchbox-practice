7c30109b1c37b3e8a30247f042e5bcd164f777d5
Jeremiah Black <jeremiahblackol@gmail.com>
2019-12-26 10:56:05 -0700

    2 hour, better next time

--- lunchbox.js
+++ lunchbox.js
@@ -1,5 +1,26 @@
 class LunchBox {
+  constructor(lunchboxInfo) {
+    this.owner = lunchboxInfo.owner;
+    this.material = lunchboxInfo.madeOf;
+    this.shape = lunchboxInfo.shape;
+    this.color = lunchboxInfo.color;
+    this.handle = true;
+    this.snacks = [];
 
+  }
+  acquireSnack(fruitSnack){
+    this.snacks.push(fruitSnack);
+    fruitSnack.isInLunchBox = true;
+  }
+  findHealthySnacks(snacks){
+    var fruitySnacks = [];
+    for(var i = 0; i < this.snacks.length; i++) {
+      if(this.snacks[i].healthy === true) {
+        fruitySnacks.push(this.snacks[i]);
+      }
+    }
+        return fruitySnacks;
+  }
 }
 
 module.exports = LunchBox;
--- snack.js
+++ snack.js
@@ -1,5 +1,18 @@
 class Snack {
-
+  constructor(type){
+    this.deliciousLevel = 'extra';
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+    this.healthy = this.type.includes('Fruit')
+  }
+  getEaten(){
+    this.amount = this.amount - 10;
+    if(this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  }
 }
 
 module.exports = Snack;
